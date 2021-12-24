600d5ff4689e3056d80bb44480a79c6a146f04c7
Seung-wan <tmddhks0104@gmail.com>
2021-12-25 01:00:08 +0900

    create vue-app

---
+++ vue-app/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <TodoList />
+</template>
+
+<script>
+import TodoList from './components/TodoList';
+export default {
+  name: 'App',
+  components: { TodoList },
+};
+</script>
+
+<style></style>
---
+++ vue-app/src/components/TodoList.vue
@@ -0,0 +1,14 @@
+<template lang="ko">
+  <div>
+  <h3>TodoList</h3>
+  <p>
+    <h3>title : react</h3> 
+    content : next.js
+  </p>
+  </div>
+
+</template>
+<script>
+export default { name: 'TodoList' };
+</script>
+<style></style>
